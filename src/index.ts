2b81104e399e7f03db19aba3bea332e8a5613c8d
vynguyn <vy21014nguyn@gmail.com>
2023-02-08 12:21:18 -0600

    Update index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 /*
  *   index.ts
- *   Project: Second Assignment
+ *   Project: Second Assignment Part 1 and 2
  *
  *   Author: Tran Nguyen
  *   Created on: February 3, 2023
@@ -10,18 +10,31 @@
 import express, { Express } from 'express';
 
 // Import functions from StudentController
-import StudentController from './controllers/StudentController';
+import {
+  getAllStudents,
+  createNewStudent,
+  getStudentByName,
+  getFinalExamScores,
+  calcFinalScore,
+  updateGrade,
+} from './controllers/StudentController';
 
 // Create your app object
 const app: Express = express();
-const PORT = 8000;
+const PORT = 8080;
 
 // Activate json parsing for the request body
 app.use(express.json());
 
 // Register your route handlers for the specified endpoints
-app.post('/api/students', StudentController.createNewStudent);
-app.get('/api/students/:studentName', StudentController.getStudentByName);
+app.get('/', getAllStudents);
+app.post('/students', createNewStudent);
+app.get('/students/:studentName', getStudentByName);
+app.get('/students/:studentName/finalExam', getFinalExamScores);
+app.post('/students/:studentName/finalExam', calcFinalScore);
+app.post('/students/:studentName/grades/:assignmentName', updateGrade);
 
 // Start listening on the chosen port
-app.listen(PORT, () => console.log(`Listening on port http://127.0.0.1:${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Listening on port http://127.0.0.1:${PORT}`);
+});
