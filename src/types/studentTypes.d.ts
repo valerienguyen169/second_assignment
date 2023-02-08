8adc25bef17e30658220fe40039aa74ba3635b73
vynguyn <vy21014nguyn@gmail.com>
2023-02-08 12:23:13 -0600

    Add new types to the file

--- src/types/studentTypes.d.ts
+++ src/types/studentTypes.d.ts
@@ -1,39 +1,48 @@
 type CourseGrades = {
-    assignmentWeights: Array<CourseGrade>,
-    finalExamWeight: number,
+  assignmentWeights: Array<CourseGrade>;
+  finalExamWeight: number;
 };
 
 type CourseGrade = {
-    name: string,
-    weight: number,
-    grade: number,
+  name: string;
+  weight: number;
+  grade: number;
 };
 
 type Student = {
-    name: string,
-    weights: CourseGrades,
-    currentAverage: number,
+  name: string;
+  weights: CourseGrades;
+  currentAverage: number;
 };
 
 type NewStudentRequest = {
-    name: string,
-    weights: CourseGrades,
+  name: string;
+  weights: CourseGrades;
 };
 
 type AssignmentGrade = {
-    grade: number,
+  grade: number;
 };
 
 type FinalGrade = {
-    overallScore: number,
-    letterGrade: string,
+  overallScore: number;
+  letterGrade: string;
 };
 
 type FinalExamScores = {
-    neededForA:	number,
-    neededForB:	number,
-    neededForC:	number,
-    neededForD:	number,
+  neededForA: number;
+  neededForB: number;
+  neededForC: number;
+  neededForD: number;
+};
+
+type StudentNameParams = {
+  studentName: string;
+};
+
+type GradeUpdateParams = {
+  studentName: string;
+  assignmentName: string;
 };
 
 type StudentManager = Record<string, Student>;
